3c49ddf7fce6c256c3c1e470db951a5e7d390f16
mikepham <mikepham137@gmail.com>
2022-06-21 22:40:40 +0700

    Basic React lesson

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
-import HandleEvent from './Component/HandleEvent';
-import RenderingCondition from './Component/RenderingCondition';
+import HandleEvent from './Component/HandleEvent/HandleEvent';
+import RenderingCondition from './Component/RenderCondition/RenderingCondition';
+import State from './Component/StateAndSetState/State';
+import Styling from './Component/StylingCSS/Styling';
 
 function App() {
   return (
     <div className="App">
       <HandleEvent />
       <RenderingCondition/>
+      <State/>
+      <Styling/>
     </div>
   );
 }
